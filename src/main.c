343740ce8336799878b7867e70aaa53f4ea6484d
Mauricio Bernardini <mausissio@gmail.com>
2017-11-07 00:29:47 -0200

    Falta o teste 04, não sei oq há

--- src/main.c
+++ src/main.c
@@ -9,9 +9,41 @@
 
 int main() {
 
-  int x, y;
+  int n=0;
+  int i;
+  int j = 0;
+  int vetor [20000];
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+  FILE *pipe_sort;
+  pipe_sort = popen("sort -n -r", "w");
+
+  while (vetor[j]!= -1)
+	{
+	  j++;
+	  scanf("%d", &vetor[j]);
+	}
+  for(int cont = 0; cont<j ; cont++)
+	{
+	  if(vetor[cont]==1)
+		{
+		  fprintf(pipe_sort, "%d\n", vetor[cont]);
+		}
+	  i=2;
+	  for(i=2;i<=(vetor[cont]/2);i++)
+		{
+	  	  if(vetor[cont]%i==0)
+			{
+		  	  fprintf(pipe_sort, "%d\n", vetor[cont]);
+		  	  break;
+			}
+		}
+	  if(vetor[cont]==-1)
+		{
+		  break;
+		}
+	}
+
+
+  pclose(pipe_sort);	
   return 0;
 }
